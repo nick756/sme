400a55981b66315634aa53f9bcbc793069c7ba15
Nikolay Krasnikov <Nikolay Krasnikov@Lenovo-PC>
2015-07-01 12:46:22 +0800

    Session control for mobile users

---
+++ src/groovy/com/sme/util/MobileUser.groovy
@@ -0,0 +1,14 @@
+package com.sme.util
+
+import com.sme.entities.User
+
+/**
+ *
+ * @author Nikolay Krasnikov
+ */
+class MobileUser {
+    Date loggedTime
+    Date lastOperation
+    User user
+}
+
